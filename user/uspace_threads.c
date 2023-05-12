6b31e04201de49a7c6726ca8e3d914b623a43eaf
root <root@RHIT-R912GACJ.localdomain>
2023-05-12 09:31:31 -0400

    c file made

---
+++ user/uspace_threads.c
@@ -0,0 +1,16 @@
+/*
+ * Userspace Threads
+ *
+ *
+ */
+#include <uspace_threads.h>
+
+void makecontext(ucontext_t thread, void* func, void* args){
+
+
+}
+
+void swapcontext(ucontext_t oldThread, ucontext_t newThread) {
+
+
+}
