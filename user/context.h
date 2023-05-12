28569434a9fd978d3f540401b608f6241f97d725
root <root@RHIT-R912GACJ.localdomain>
2023-05-12 09:04:46 -0400

    header files maybe done

---
+++ user/context.h
@@ -0,0 +1,10 @@
+typedef unsigned long int greg_t;
+
+typedef struct gregset
+   {
+     greg_t gregs[14];
+   } gregset_t;
+
+typedef struct {
+	gregset_t regset;
+} mcontext_t;
--- user/ucontext.h
+++ user/ucontext.h
@@ -1,9 +1,11 @@
 #include <signal.h>
-#include <struct.h>
+#include <stack.h>
 #include <context.h>
 
 
 typedef struct {
-
-
+	stack_t uc_stack;
+	mcontext_t uc_mcontext;
+	int id;
+	void* fn_ptr;
 }ucontext_t;
