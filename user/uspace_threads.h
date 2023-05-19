8e703fcaeb7ffaea0dac69cc6777e7b1ba4933b0
root <root@RHIT-R912GACJ.localdomain>
2023-05-19 07:37:51 -0400

    some small changes

--- user/uspace_threads.h
+++ user/uspace_threads.h
@@ -4,18 +4,10 @@
 
 typedef uint64 greg_t;
 
-//rewrite gregs into 14 different pointers for each register
-
 typedef struct gregset
    {
      greg_t gregs[14];
    } mcontext_t;
-
-// typedef struct {
-//         void  *ss_sp;     /* Base address of stack */
-//         int    ss_flags;  /* Flags */
-// 	size_t ss_size;   /* Number of bytes in stack */
-// } stack_t;
 
 typedef struct {
 	void* uc_stack;
@@ -26,8 +18,8 @@ typedef struct {
 }ucontext_t;
  
 void swapcontext(ucontext_t* oldThread, ucontext_t* newThread);
-void makecontext(ucontext_t* thread, void* func
-//, void* argv1 = 0, void* argv2 = 0, void* argv3 = 0, void* argv4 = 0, void* argv5 = 0, void* argv6 = 0, void* argv7 = 0, void* argv8 = 0
+void makecontext(ucontext_t* thread, void* func , void* argv1 = 0, void* argv2 = 0, void* argv3 = 0, 
+    void* argv4 = 0, void* argv5 = 0, void* argv6 = 0, void* argv7 = 0, void* argv8 = 0
 );
 
 
